ebd78173e1e381b434f824f826ce75454378de37
Erik Abair <erik.abair@gmail.com>
2026-04-22 22:58:37 -0700

    Fixes Linux build.

--- src/main.cpp
+++ src/main.cpp
@@ -270,12 +270,12 @@ int main(int, char**) {
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #else
-  // GL 3.0 + GLSL 130
+  // GL 3.3 + GLSL 130
   const char* glsl_version = "#version 130";
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 #endif
 
   // Create window with graphics context
